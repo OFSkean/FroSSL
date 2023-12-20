a3affecefd69421ed30b3a525b5c8fb4af8be6ba
Oscar Skean <ofskean727@gmail.com>
2023-12-20 13:43:12 -0500

    shell scripts

---
+++ linear.sh
@@ -0,0 +1,3 @@
+python3 main_linear.py \
+    --config-path scripts/linear/cifar \
+    --config-name logless.yaml \
---
+++ run.sh
@@ -0,0 +1,3 @@
+python3 main_pretrain.py \
+    --config-path scripts/pretrain/stl10 \
+    --config-name corinfomax.yaml \
