ed1519f111f35e7d0de5d9bf66de6d74bd21c3af
Oscar Skean <ofskean727@gmail.com>
2024-01-19 23:39:56 -0500

    dont log gradients

--- main_pretrain.py
+++ main_pretrain.py
@@ -205,7 +205,7 @@ def main(cfg: DictConfig):
             resume="allow" if wandb_run_id else None,
             id=wandb_run_id,
         )
-        wandb_logger.watch(model, log="gradients", log_freq=100)
+        #wandb_logger.watch(model, log="gradients", log_freq=100)
         wandb_logger.log_hyperparams(OmegaConf.to_container(cfg))
 
         # lr logging
