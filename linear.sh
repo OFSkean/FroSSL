d80570b460a9221235184730a175727428d08923
Oscar Skean <ofskean727@gmail.com>
2024-01-30 18:20:07 -0500

    updated linear probe script

--- linear.sh
+++ linear.sh
@@ -5,20 +5,22 @@ then
 fi
 
 EXPERIMENT_NAME="$1"
-ENTROPY_CUTOFF="$2"
-TRAINED_CHECKPOINT_PATH="$3"
-DATASET="stl10"
+DATASET="cifar100"
 CONFIG_NAME="mmcr.yaml"
 
-LINEAR_CONFIG_PATH="scripts/linear/$DATASET"
-TRAINED_CHECKPOINT_WANDB_ID=$(echo $TRAINED_CHECKPOINT_PATH | awk -F '-' '{print $4}')
+TRAINED_CHECKPOINT_PATH="/home/AD/ofsk222/Research/clones/solo-learn/trained_models/mmcr/urna7xcz/mmcr-cifar10-urna7xcz-ep=999.ckpt"
+
+# check if data is cifar
+if [ "$DATASET" == "cifar10" ] || [ "$DATASET" == "cifar100" ]; then
+    dataset_config_name="cifar"
+else
+    dataset_config_name="$DATASET"
+fi
+LINEAR_CONFIG_PATH="scripts/linear/$dataset_config_name"
 
 echo "Preparing to start linear probe for experiment with name $EXPERIMENT_NAME"
 echo "on dataset $DATASET"
-echo "with config $CONFIG_NAME"
-echo "with entropy cutoff $ENTROPY_CUTOFF"
-echo "checkpoint path $TRAINED_CHECKPOINT_PATH"
-echo "checkpoint wandbid $TRAINED_CHECKPOINT_WANDB_ID"
+echo "with config $LINEAR_CONFIG_PATH/$CONFIG_NAME"
 
 echo $TRAINED_CHECKPOINT_PATH > last_ckpt.txt
 
@@ -28,4 +30,6 @@ echo $TRAINED_CHECKPOINT_PATH > last_ckpt.txt
 CUDA_LAUNCH_BLOCKING=1 python3 -u main_linear.py \
     --config-path $LINEAR_CONFIG_PATH \
     --config-name $CONFIG_NAME \
-    ++name="$EXPERIMENT_NAME-linear-$TRAINED_CHECKPOINT_WANDB_ID" \
+    ++name="$EXPERIMENT_NAME-linear-urna7xcz" \
+    ++data.dataset="$DATASET" \
+    ++precompute=True \
